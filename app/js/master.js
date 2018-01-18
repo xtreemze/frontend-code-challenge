69676748b10abfc6c812eb515039895f841906b7
xtreemze <carlos@2activedesign.com>
2018-01-18 12:14:45 +0100

    add card class

--- app/js/master.js
+++ app/js/master.js
@@ -9,3 +9,26 @@ let fetchedListings = fetch("https://api.mcmakler.de/v1/advertisements")
   .then(function(data) {
     listings.innerHTML = data;
   });
+
+
+  class card {
+    constructor(arguments) {
+      this.cardHtml = `
+      
+  <div class="row">
+  <div class="col s12 m6">
+    <div class="card">
+      <div class="card-image">
+        <img src="${sourceImage}">
+        <span class="card-title">${cardTitle}</span>
+        <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+      </div>
+      <div class="card-content">
+${cardContent}
+      </div>
+    </div>
+  </div>
+</div>
+      `
+    }
+  }
