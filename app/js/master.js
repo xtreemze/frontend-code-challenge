597000d09e9bb8eab751c1489ef9d3b570bf636d
xtreemze <carlos@2activedesign.com>
2018-01-18 13:18:25 +0100

    corrections

--- app/js/master.js
+++ app/js/master.js
@@ -1,28 +1,15 @@
-window.addEventListener("load", function() {});
-
-let listings = document.getElementById("listings");
-let resultJson;
-
-let fetchedListings = fetch("https://api.mcmakler.de/v1/advertisements?callback=JSONP_CALLBACK",  {'mode': 'no-cors'})
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    resultJson = data.data;
-  });
-
-
-  class card {
-    constructor(arguments) {
-this.sourceImage = arguments.advertisementAssets.advertisementThumbnails.inventory_m.url;
-this.cardTitle = arguments.title;
-this.priceRent = arguments.advertisementPrice.baseRent;
-this.priceSell = arguments.advertisementPrice.sellprice;
-this.furnished = arguments.hasFurniture;
-this.rooms = arguments.realestateSummary.numberOfRooms;
-this.address = arguments.realestateSummary.address;
-this.space = arguments.realestateSummary.space;
-      this.cardHtml = `<div class="row">
+class card {
+  constructor(args) {
+    this.sourceImage =
+      args.advertisementAssets.advertisementThumbnails.inventory_m.url;
+    this.cardTitle = args.title;
+    this.priceRent = args.advertisementPrice.baseRent;
+    this.priceSell = args.advertisementPrice.sellprice;
+    this.furnished = args.hasFurniture;
+    this.rooms = args.realestateSummary.numberOfRooms;
+    this.address = args.realestateSummary.address;
+    this.space = args.realestateSummary.space;
+    this.cardHtml = `<div class="row">
   <div class="col s12 m6">
     <div class="card">
       <div class="card-image">
@@ -30,7 +17,7 @@ this.space = arguments.realestateSummary.space;
         <span class="card-title">${this.cardTitle}</span>
       </div>
       <div class="card-content">
-        ${cardContent}
+        ${this.cardContent}
       </div>
       <div class="price">
         ${this.priceRent} ${this.priceSell}
@@ -46,23 +33,33 @@ this.space = arguments.realestateSummary.space;
     </div>
   </div>
 </div>
-`
-listings.innerHTML += this.card;
-    }
+`;
+    listings.innerHTML += this.cardHtml;
   }
+}
 
+let listings = document.getElementById("listings");
+window.resultJson = {};
 
+// fetch(
+//   "https://api.mcmakler.de/v1/advertisements?callback=JSONP_CALLBACK",
+//   { mode: "no-cors" }
+// )
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(data) {
+//     resultJson = data.data;
+//   });
 
-  if (resultJson.length < 10){
-    const importCines = function() {
-      const imported = require("./advertisements.json");
-      imported.data.forEach(function(item) {
-        new card(item);
-      });
-    };
-  } else {
-    for (i = 0; i < 10; i ++){
-      new card(resultJson.data[i]);
-     }
-  }
-
+if (window.resultJson.length < 10) {
+  const imported = require("./advertisements.json");
+  imported.data.forEach(function(item) {
+    new card(item);
+  });
+}
+//  else {
+//   for (let i = 0; i < 10; i++) {
+//     new card(resultJson.data[i]);
+//   }
+// }
