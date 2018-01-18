2dcc1fc7cef57706a79bd3b350ea3f4d3beef244
xtreemze <carlos@2activedesign.com>
2018-01-18 14:46:36 +0100

    offline support

--- app/js/offlineRuntimeInstall.js
+++ app/js/offlineRuntimeInstall.js
@@ -3,40 +3,16 @@ import M from "materialize-css";
 // require("./../../node_modules/materialize-css/dist/js/materialize");
 
 OfflinePluginRuntime.install({
-  onInstalled: function() {
-    M.toast({
-      html: "App Installed",
-      displayLength: 500,
-      classes: "blue darken-2"
-    });
-  },
+  onInstalled: function() {},
 
-  onUpdating: function() {
-    M.toast({
-      html: "Updating...",
-      displayLength: 500,
-      classes: "blue darken-2"
-    });
-  },
+  onUpdating: function() {},
 
   onUpdateReady: function() {
-    const foot = document.getElementById("foot");
     OfflinePluginRuntime.applyUpdate();
-    foot.classList.add("fadeIn");
-    M.toast({
-      html: "Update Ready",
-      displayLength: 500,
-      classes: "blue darken-2"
-    });
   },
   onUpdated: function() {
     setTimeout(function() {
       window.location.reload();
     }, 500);
-    M.toast({
-      html: "Reloading",
-      displayLength: 500,
-      classes: "blue darken-2"
-    });
   }
 });
