149abc64b536982c05b5d725ecad6638ac3bd611
xtreemze <carlos@2activedesign.com>
2018-01-18 12:31:33 +0100

    add content

--- app/js/master.js
+++ app/js/master.js
@@ -1,34 +1,48 @@
 window.addEventListener("load", function() {});
 
 let listings = document.getElementById("listings");
+let resultJson;
 
 let fetchedListings = fetch("https://api.mcmakler.de/v1/advertisements")
   .then(function(response) {
     return response.json();
   })
   .then(function(data) {
-    listings.innerHTML = data;
+    resultJson = data.data;
   });
 
 
   class card {
     constructor(arguments) {
-      this.cardHtml = `
-      
-  <div class="row">
+this.sourceImage = arguments.advertisementAssets[0].advertisementThumbnails.inventory_m.url;
+this.cardTitle = arguments.title;
+this.priceRent = arguments.advertisementPrice.baseRent;
+this.priceSell = arguments.advertisementPrice.sellprice;
+this.furnished = arguments.hasFurniture;
+      this.cardHtml = `<div class="row">
   <div class="col s12 m6">
     <div class="card">
       <div class="card-image">
-        <img src="${sourceImage}">
-        <span class="card-title">${cardTitle}</span>
-        <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+        <img src=${this.sourceImage}>
+        <span class="card-title">${this.cardTitle}</span>
+        <a class="btn-floating halfway-fab waves-effect waves-light red">
+          <i class="material-icons">add</i>
+        </a>
       </div>
       <div class="card-content">
-${cardContent}
+        ${cardContent}
+      </div>
+      <div class="price">
+${this.priceRent}
+${this.priceSell}
       </div>
     </div>
   </div>
 </div>
-      `
+`
     }
   }
+
+  for (i = 0; i < 10; i ++){
+    new card(resultJson.data[i]);
+   }
